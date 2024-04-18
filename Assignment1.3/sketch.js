7ddd19eda475442d125aee22b0e49e9b20060b40
Marshall <mmccur6@lsu.edu>
2024-04-18 15:32:35 -0500

    completed

--- Assignment1.3/sketch.js
+++ Assignment1.3/sketch.js
@@ -1,65 +1,156 @@
+const CANVAS_WIDTH  = 700;
+const CANVAS_HEIGHT = 500;
+const CANVAS_GROUND_POSITION = 400;
+const CANVAS_GROUND_THICKNESS = 10;
+const SPRITE_SIZE = 80;
+
+// move the character down by the specified number
+// of pixels, to make the character's feet appear
+// to be touching the ground
+const CHARACTER_DISPLACEMENT = 8;
+
+const FRAME_RATE = 12;
+const CHARACTER_SPEED = 5.0;
+
+const NUM_CHARACTERS = 3;
+
 let characters = [];
-let spriteSheet; // Assume this is your sprite sheet for a character
+
+const SPRITES_PREFIX = "sprites/";
+
+const spriteSheets = [
+  "red.png",
+  "round_girl.png",
+  "eskimo.png"
+];
+
+class Character {
+
+  constructor( posX, direction, spriteSheet ) {
+
+    this.posX = posX;
+
+    // -1 means character is facing left
+    // +1 means character is facing right
+    this.direction = direction;
+
+    this.spriteSheet = loadImage( SPRITES_PREFIX + spriteSheet );
+
+    // this number stores the current frame of the
+    // animation, between 0 and 8
+    this.animFrame = 0;
+
+    // whether the character is standing still or moving
+    this.standingStill = true;
+  }
+
+  // update position of character for the next frame
+  update( direction ) {
+  
+    if ( direction == 0 ) {
+      // make character stand still
+      this.standingStill = true;
+    }
+    else {
+      // make character not stand still
+      this.standingStill = false;
+
+      // make character point in the correct direction
+      this.direction = direction;
+    }
+
+    if ( direction != 0 ) {
+      // update position based on direction
+      this.posX += direction * CHARACTER_SPEED;
+
+      // clamp coordinate to canvas
+      if ( this.posX < 0 ) {
+        this.posX = 0;
+      }
+      if ( this.posX >= CANVAS_WIDTH ) {
+        this.posX = CANVAS_WIDTH - 1;
+      }
+    }
+  }
+
+  // draw the character with the next animation frame
+  draw() {
+
+    if ( this.standingStill ) {
+      // reset animation frame to standing still
+      this.animFrame = 0;
+    }
+    else {
+      // go to next animation frame
+      if ( ++this.animFrame == 8 ) {
+        this.animFrame = 0;
+      }
+    }
+
+    // get the current animation frame from sprite sheet
+    let frame = this.spriteSheet.get(
+      this.animFrame * SPRITE_SIZE, 0,
+      SPRITE_SIZE, SPRITE_SIZE
+    );
+
+    // draw the current animation frame
+    push();
+    translate(
+      this.posX,
+      CANVAS_GROUND_POSITION - ( SPRITE_SIZE - CHARACTER_DISPLACEMENT )
+    );
+    //if facing left, mirror the image
+    if ( this.direction == -1 )
+      scale( -1, +1 );
+    image( frame, -0.5 * SPRITE_SIZE, 0, SPRITE_SIZE, SPRITE_SIZE );
+    pop();
+  }
+}
 
 function preload() {
-  // Load your sprite sheets here
-  spriteSheet = loadImage('path/to/spriteSheet.png');
+  // preload all sprite sheets
+  for ( let spriteSheet of spriteSheets ) {
+    spriteSheet = loadImage( SPRITES_PREFIX + spriteSheet );
+  };
 }
 
 function setup() {
-  createCanvas(800, 600);
-  // Initialize your characters here
-  for (let i = 0; i < 3; i++) { // Example for 3 characters
-    characters.push(new Character(random(width), random(height), spriteSheet));
+
+  // for every sprite sheet, create a characters with random
+  // position and direction
+  for ( let spriteSheet of spriteSheets ) {
+    let posX = Math.floor( Math.random() * CANVAS_WIDTH );
+    let direction;
+    if ( Math.round( Math.random() ) == 0 )
+      direction = -1;
+    else
+      direction = +1;
+    characters.push( new Character( posX, direction, spriteSheet ) );
   }
+
+  // set up canvas
+  createCanvas( CANVAS_WIDTH, CANVAS_HEIGHT );
+
+  // set frame rate
+  frameRate( FRAME_RATE );
 }
 
 function draw() {
-  background(255);
-  // Update and display your characters here
-  for (let char of characters) {
-    char.update();
-    char.display();
-  }
-}
+  background(220);
 
-function keyPressed() {
-  // Handle left and right arrow keys
-  if (keyCode === RIGHT_ARROW) {
-    // Move characters right
-    for (let char of characters) {
-      char.moveRight();
-    }
-  } else if (keyCode === LEFT_ARROW) {
-    // Move characters left
-    for (let char of characters) {
-      char.moveLeft();
-    }
-  }
-}
+  // draw the ground
+  noStroke();
+  fill( 'brown' );
+  rect( 0, CANVAS_GROUND_POSITION, CANVAS_WIDTH, CANVAS_GROUND_THICKNESS );
 
-// Character class
-class Character {
-  constructor(x, y, spriteSheet) {
-    this.x = x;
-    this.y = y;
-    this.spriteSheet = spriteSheet;
-    // Add more properties as needed for animation
-  }
-
-  update() {
-    // Update character state and animation frame
-  }
-
-  display() {
-    // Draw character based on current state and frame
-  }
-
-  moveRight() {
-    // Move character right
-  }
-
-  moveLeft() {
-    // Move character left using scale() to flip the sprite
+  // update and draw all characters
+  let direction = 0;
+  if ( keyIsDown( LEFT_ARROW ) )
+    direction--;
+  if ( keyIsDown( RIGHT_ARROW ) )
+    direction++;
+  for ( let character of characters ) {
+      character.update( direction );
+      character.draw();
   }
 }
